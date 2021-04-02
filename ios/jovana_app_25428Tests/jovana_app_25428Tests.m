2d40eed810c313a8c9f7dcd3b4794cedf946d871
Crowdbotics <team@crowdbotics.com>
2021-04-02 15:44:12 +0000

    Initial commit.

---
+++ ios/jovana_app_25428Tests/jovana_app_25428Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface jovana_app_25428 : XCTestCase
+
+@end
+
+@implementation jovana_app_25428
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://jovana-app-25428.botics.co",
+  defaultTimeout: 5000
+};
